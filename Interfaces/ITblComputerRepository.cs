13073b213e9bc6e642612c88c4171e4b1a9fc5de
lapykappa <131544432+lapykappa@users.noreply.github.com>
2023-10-25 11:23:58 +0200

    Add files via upload

---
+++ Interfaces/ITblComputerRepository.cs
@@ -0,0 +1,14 @@
+﻿using DbFirstCIS2.Models;
+using DbFirstCIS2.DTO;
+namespace DbFirstCIS2.Interfaces
+{
+    public interface ITblComputerRepository
+    {
+        Task<IEnumerable<ComputerDto>> GetAllAsync();
+        Task<ComputerDto> GetAsync(int id);
+        Task<ComputerDto> CreateAsync(ComputerDto computerDto);
+        Task UpdateAsync(ComputerDto computerDto);
+        Task DeleteAsync(int id);
+    }
+
+}
