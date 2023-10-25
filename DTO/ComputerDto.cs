c08e08d3390fdf33713690e4f1fd21dc8b5cd259
lapykappa <131544432+lapykappa@users.noreply.github.com>
2023-10-25 11:23:49 +0200

    Add files via upload

---
+++ DTO/ComputerDto.cs
@@ -0,0 +1,16 @@
+﻿namespace DbFirstCIS2.DTO
+{
+    public class ComputerDto
+    {
+        public int Id { get; set; }
+        public string ComputerName { get; set; }
+        public string TypeDescription { get; set; } 
+        public string FunctionalityDescription { get; set; }
+        public string SerialNr { get; set; }
+        public string Inventory { get; set; }
+        public string Comment { get; set; }
+    }
+
+}
+
+
